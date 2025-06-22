0cbfd914bf36056258d1ba0e680a4bb81c45314b
KevinCalderon <elkevin2811@gmail.com>
2025-06-21 23:15:47 -0600

    Cambios
    Trabajé avances en el Veterinario. Y un poco también la GUI. Es un boceto.

--- Proyecto2/InterfazGrafica.java
+++ Proyecto2/InterfazGrafica.java
@@ -1,15 +1,98 @@
 import javax.swing. *;
 
 public class InterfazGrafica extends JFrame{
-    private ColaMascota cola = new ColaMascota();
+    private Veterinario veterinario = new Veterinario();
 
+
+    //El constructor donde va todito
     public InterfazGrafica(){
         setTitle("Clinica Veterinaria");
         setSize(1000, 1000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null); 
 
-    }
+        //PANEL PRINCIPAL
+
+        JPanel panelPrincipal = new JPanel();
+        panelPrincipal.setLayout(new GridLayout(3, 2));
+
+        //PANEL DE DATOS
+
+        JPanel panelDatos = new JPanel();
+        panelDatos.setLayout(new BoxLayout(panelDatos, BoxLayout.Y_AXIS));
+
+        //LO Q ESTA CONTENIDO EN EL PANELDATOS
+
+        JLabel eNombre = new JLabel("Nombre: ");
+        JTextField cNombre = new JTextField(15);
+        JLabel eId = new JLabel("Id: ");
+        JTextField cId = new JTextField(15);
+        JLabel eEspecie = new JLabel("Especie: ");
+        JTextField cEspecie = new JTextField(15);
+        
+        //BOTON PARA REGISTRAR MASCOTAS
+        JButton botonRegistrar = new JButton("Registrar");
+        botonRegistrar.addActionListener(e -> {
+
+        //Agregamos los datos
+
+        String nombre = cNombre.getText();
+        String idTexto = cId.getText();
+        String especie = cEspecie.getText();
+
+            //Verificamos que no esten vacios y que el id no contenga letras.
+
+            if(nombre.isEmpty() || idTexto.isEmpty() || especie.isEmpty()){
+                JOptionPane.showMessageDialog(null, "Por favor, rellene todos los datos de la mascota a registrar.",  "Advertencia", JOptionPane.WARNING_MESSAGE);
+                return; //Paramos el codigo, para que no nos salga el otro mensaje de error.
+            }
+            try{
+                int id = Integer.parseInt(idTexto);
+                Mascota mascotaRegistrada = veterinario.registrar(nombre, id, especie);
+                JOptionPane.showMessageDialog(null, "Datos guardados exitosamente.", "Mensahe", JOptionPane.INFORMATION_MESSAGE);
+
+            }catch(NumberFormatException ex){
+                JOptionPane.showMessageDialog(null, "Rellene el id con numeros.", "Advertencia", JOptionPane.WARNING_MESSAGE);
+            }
+            
+            
+        });
+
+        //Agregamos los elementos al panelDatos
+        panelDatos.add(eNombre);
+        panelDatos.add(cNombre);
+        panelDatos.add(eId);
+        panelDatos.add(cId);
+        panelDatos.add(eEspecie);
+        panelDatos.add(cEspecie);
+        panelDatos.add(botonRegistrar);
+
+        //PANEL DE LA COLA
+
+        JPanel panelCola = new JPanel();
+        panelCola.setLayout(new BoxLayout(panelCola, BoxLayout.Y_AXIS));
+
+        String textoCola = veterinario.mostrarColaa();
+        JTextArea areaTexto = new JTextArea(textoCola);
+        areaTexto.setEditable(false);
+        JScrollPane larry = new JScrollPane(areaTexto); 
+
+
+        //Agregamos los elementos al panel de la cola
+        panelCola.add(larry);
+
+        
+        
+        //PANEL SMDSAKDMNAKSDMKA
+
+        //Se agregan todos los paneles secundarios al principal
+        panelPrincipal.add(panelDatos);
+        panelPrincipal.add(panelCola);
+
+        //Se agrega finalmente el panelPrincipal
+        add(panelPrincipal);
+
+    }  
 
     public static void main(String[] args){
         InterfazGrafica interfaz = new InterfazGrafica();
--- Proyecto2/Veterinario.java
+++ Proyecto2/Veterinario.java
@@ -1,25 +1,72 @@
 public class Veterinario{
     private ColaMascota cola;
-    private ArbolMascota arbol;
+    //private ArbolMascota arbol;
 
+    /**
+     *En este constructor, instanciamos nuevos objetos de tipo ColaMascota, y ArbolMascota, que conforman a la clase Veterinario; va a tener estos dos atributos.
+     */
     public Veterinario(){
-
+        this.cola = new ColaMascota();
+        //this.arbol = new ArbolMascota();
     }
 
+    /**
+     * Este metodo registra una nueva mascota, la agrega al registro del arbol, y tambien la agrega a la cola.
+     * @param nombre El nombre de la mascota.
+     * @param id el id de la misma.
+     * @param especie la especie de la mascota que va a ser registrada.
+     * @return a la mascota registrada en la cola y guardada en el arbol, null si la mascota ya se encuentra en el arbol, esto se verifica con el metodo de buscar a la mascota por la id.
+     */
     public Mascota registrar(String nombre, int id, String especie){
-
-    }
-    //metodo cola
-    public ColaMascota atender(){
-
-    }
-    //metodo arbol
-    public ArbolMascota verHistorial(){
-
-    }
-
-    public ArbolMascota mostrarMascotas(){
+        Mascota nueva = new Mascota(id, nombre, especie);
+        //Verificar q no se encuentre ya en el arbol
         
+        cola.enqueue(nueva);
+        return nueva;
     }
+   
+    
+    //Los siguientes metodos tienen que ver unicamente con la cola
+
+    /**
+     * Este metodo tiene una logica similar al metodo de dequeue de la clase ColaMascota. Se encarga de llamar al metodo de la clase mencionada, para retornar a la mascota atendida de la cabeza de la cola.
+     * @return un atributo de tipo Masocta que corresponde a la mascota sacada de la cola con el metodo dequeue(). Null si esta misma no existe; i.e. la cola esta vacia.
+     */
+
+    public Mascota atender(){
+        return cola.dequeue();
+    } 
+
+    /**
+     * Este metodo muestra para la GUI la cola, definida ya en ColaMascota.
+     * @return de forma recursiva, toda la cola actual formada por las mascotas que llegan a la veterinaria.
+     */
+    public String mostrarColaa(){
+        return cola.mostrarCola();
+    }
+    
+
+    /**
+     * Este metodo funciona para mostrar a la mascota que se encuentra en la cabeza de la cola mediante el metodo peek de ColaMascota.
+     * @return la mascota que se encuentra en la head.
+     */
+    public Mascota mostrarAtendida(){
+        return cola.peek();
+    }
+    
+    
+    
+    //Los siguientes metodos tienen que ver unicamente con el arbol.
+
+
+    //public ArbolMascota verHistorial(){
+
+    
+
+    //public ArbolMascota mostrarMascotas(){
+        
+    
+
+   
 
 }
